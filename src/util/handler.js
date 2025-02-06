e96d62bd623acb2956b9c67d940ce1e006aab42d
XenKys <sergiotuzzo@gmail.com>
2025-02-06 22:37:18 +0100

    refactor: better handler syntax

--- src/util/handler.js
+++ src/util/handler.js
@@ -1,15 +1,7 @@
 const fs = require("fs");
 
 function getFiles(path) {
-  let files = [];
-
-  for (const filePath of fs.readdirSync(path)) {
-    const file = require(`${path}/${filePath}`);
-
-    files.push(file);
-  }
-
-  return files;
+  return fs.readdirSync(path).map((filePath) => require(`${path}/${filePath}`));
 }
 
 module.exports = {
