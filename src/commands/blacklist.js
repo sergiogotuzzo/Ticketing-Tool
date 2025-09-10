45f8137d19f81ad876c169081f3156aea04f1176
XenKys <sergiotuzzo@gmail.com>
2025-09-10 22:09:23 +0200

    refactor: edited /blacklist, no limits for users and roles

--- src/commands/blacklist.js
+++ src/commands/blacklist.js
@@ -2,15 +2,91 @@ const {
   Client,
   InteractionCallbackType,
   MessageFlags,
-  ComponentTypes,
   BitwisePermissionFlags,
+  ApplicationCommandOptionType,
+  userMention,
+  roleMention,
 } = require("disgroove");
 const Blacklist = require("../models/Blacklist");
 
 module.exports = {
   name: "blacklist",
-  description: "Manage the blacklist",
+  description: "Manage the blacklist.",
   defaultMemberPermissions: BitwisePermissionFlags.ManageChannels.toString(),
+  options: [
+    {
+      name: "users",
+      description: "Add or remove a user in the blacklist.",
+      type: ApplicationCommandOptionType.SubCommandGroup,
+      options: [
+        {
+          name: "add",
+          description: "Add a user to the blacklist.",
+          type: ApplicationCommandOptionType.SubCommand,
+          options: [
+            {
+              name: "user",
+              description: "The user to add.",
+              type: ApplicationCommandOptionType.User,
+              required: true,
+            },
+          ],
+        },
+        {
+          name: "remove",
+          description: "Remove a user from the blacklist.",
+          type: ApplicationCommandOptionType.SubCommand,
+          options: [
+            {
+              name: "user",
+              description: "The user to remove.",
+              type: ApplicationCommandOptionType.User,
+              required: true,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: "roles",
+      description: "Add or remove a role in the blacklist.",
+      type: ApplicationCommandOptionType.SubCommandGroup,
+      options: [
+        {
+          name: "add",
+          description: "Add a role to the blacklist.",
+          type: ApplicationCommandOptionType.SubCommand,
+          options: [
+            {
+              name: "role",
+              description: "The role to add.",
+              type: ApplicationCommandOptionType.Role,
+              required: true,
+            },
+          ],
+        },
+        {
+          name: "remove",
+          description: "Remove a role from the blacklist.",
+          type: ApplicationCommandOptionType.SubCommand,
+          options: [
+            {
+              name: "role",
+              description: "The role to remove.",
+              type: ApplicationCommandOptionType.Role,
+              required: true,
+            },
+          ],
+        },
+      ],
+    },
+
+    {
+      name: "list",
+      description: "Get the blacklist.",
+      type: ApplicationCommandOptionType.SubCommand,
+    },
+  ],
   /**
    *
    * @param {Client} client
@@ -28,59 +104,232 @@ module.exports = {
         rolesIDs: [],
       });
 
-    client.createInteractionResponse(interaction.id, interaction.token, {
-      type: InteractionCallbackType.ChannelMessageWithSource,
-      data: {
-        flags: MessageFlags.Ephemeral,
-        embeds: [
-          {
-            color: 0,
-            title: "Blacklist",
-            description:
-              "The blacklist is a list containing users and roles that will not have any access to the ticketing system. All users and roles will not be able to create tickets, manage panels, etc. You can add up to a maximum of 25 users and 25 roles.\n\nYou can select them from the menus below.",
-          },
-        ],
-        components: [
-          {
-            type: ComponentTypes.ActionRow,
-            components: [
+    const subCommandGroup = interaction.data.options.find(
+      (option) => option.type === ApplicationCommandOptionType.SubCommandGroup
+    );
+
+    switch (subCommandGroup?.name) {
+      case "users":
+        {
+          const subCommand = subCommandGroup.options.find(
+            (option) => option.type === ApplicationCommandOptionType.SubCommand
+          );
+
+          switch (subCommand.name) {
+            case "add":
               {
-                type: ComponentTypes.UserSelect,
-                customID: "blacklist.users.set",
-                placeholder: "Users' blacklist",
-                defaultValues:
-                  blacklistData.usersIDs.length !== 0
-                    ? blacklistData.usersIDs.map((userID) => ({
-                        id: userID,
-                        type: "user",
-                      }))
-                    : undefined,
-                minValues: 0,
-                maxValues: 25,
-              },
-            ],
-          },
-          {
-            type: ComponentTypes.ActionRow,
-            components: [
+                const userID = subCommand.options.find(
+                  (option) => option.type === ApplicationCommandOptionType.User
+                ).value;
+
+                if (blacklistData.usersIDs.includes(userID))
+                  return client.createInteractionResponse(
+                    interaction.id,
+                    interaction.token,
+                    {
+                      type: InteractionCallbackType.ChannelMessageWithSource,
+                      data: {
+                        flags: MessageFlags.Ephemeral,
+                        content: `${userMention(
+                          userID
+                        )} is already in the blacklist.`,
+                      },
+                    }
+                  );
+
+                blacklistData.usersIDs.push(userID);
+                blacklistData.save();
+
+                client.createInteractionResponse(
+                  interaction.id,
+                  interaction.token,
+                  {
+                    type: InteractionCallbackType.ChannelMessageWithSource,
+                    data: {
+                      flags: MessageFlags.Ephemeral,
+                      content: `Added ${userMention(userID)} to the blacklist.`,
+                    },
+                  }
+                );
+              }
+              break;
+            case "remove":
               {
-                type: ComponentTypes.RoleSelect,
-                customID: "blacklist.roles.set",
-                placeholder: "Roles' blacklist",
-                defaultValues:
-                  blacklistData.rolesIDs.length !== 0
-                    ? blacklistData.rolesIDs.map((roleID) => ({
-                        id: roleID,
-                        type: "role",
-                      }))
-                    : undefined,
-                minValues: 0,
-                maxValues: 25,
-              },
-            ],
-          },
-        ],
-      },
-    });
+                const userID = subCommand.options.find(
+                  (option) => option.type === ApplicationCommandOptionType.User
+                ).value;
+
+                if (!blacklistData.usersIDs.includes(userID))
+                  return client.createInteractionResponse(
+                    interaction.id,
+                    interaction.token,
+                    {
+                      type: InteractionCallbackType.ChannelMessageWithSource,
+                      data: {
+                        flags: MessageFlags.Ephemeral,
+                        content: `${userMention(
+                          userID
+                        )} is not in the blacklist.`,
+                      },
+                    }
+                  );
+
+                blacklistData.usersIDs = blacklistData.usersIDs.filter(
+                  (id) => id != userID
+                );
+                blacklistData.save();
+
+                client.createInteractionResponse(
+                  interaction.id,
+                  interaction.token,
+                  {
+                    type: InteractionCallbackType.ChannelMessageWithSource,
+                    data: {
+                      flags: MessageFlags.Ephemeral,
+                      content: `Removed ${userMention(
+                        userID
+                      )} from the blacklist.`,
+                    },
+                  }
+                );
+              }
+              break;
+          }
+        }
+        break;
+      case "roles":
+        {
+          const subCommand = subCommandGroup.options.find(
+            (option) => option.type === ApplicationCommandOptionType.SubCommand
+          );
+
+          switch (subCommand.name) {
+            case "add":
+              {
+                const roleID = subCommand.options.find(
+                  (option) => option.type === ApplicationCommandOptionType.Role
+                ).value;
+
+                if (blacklistData.rolesIDs.includes(roleID))
+                  return client.createInteractionResponse(
+                    interaction.id,
+                    interaction.token,
+                    {
+                      type: InteractionCallbackType.ChannelMessageWithSource,
+                      data: {
+                        flags: MessageFlags.Ephemeral,
+                        content: `${roleMention(
+                          roleID
+                        )} is already in the blacklist.`,
+                      },
+                    }
+                  );
+
+                blacklistData.rolesIDs.push(roleID);
+                blacklistData.save();
+
+                client.createInteractionResponse(
+                  interaction.id,
+                  interaction.token,
+                  {
+                    type: InteractionCallbackType.ChannelMessageWithSource,
+                    data: {
+                      flags: MessageFlags.Ephemeral,
+                      content: `Added ${roleMention(roleID)} to the blacklist.`,
+                    },
+                  }
+                );
+              }
+              break;
+            case "remove":
+              {
+                const roleID = subCommand.options.find(
+                  (option) => option.type === ApplicationCommandOptionType.Role
+                ).value;
+
+                if (!blacklistData.rolesIDs.includes(roleID))
+                  return client.createInteractionResponse(
+                    interaction.id,
+                    interaction.token,
+                    {
+                      type: InteractionCallbackType.ChannelMessageWithSource,
+                      data: {
+                        flags: MessageFlags.Ephemeral,
+                        content: `${roleMention(
+                          roleID
+                        )} is not in the blacklist.`,
+                      },
+                    }
+                  );
+
+                blacklistData.rolesIDs = blacklistData.rolesIDs.filter(
+                  (id) => id != roleID
+                );
+                blacklistData.save();
+
+                client.createInteractionResponse(
+                  interaction.id,
+                  interaction.token,
+                  {
+                    type: InteractionCallbackType.ChannelMessageWithSource,
+                    data: {
+                      flags: MessageFlags.Ephemeral,
+                      content: `Removed ${roleMention(
+                        roleID
+                      )} from the blacklist.`,
+                    },
+                  }
+                );
+              }
+              break;
+          }
+        }
+        break;
+    }
+
+    const subCommand = interaction.data.options.find(
+      (option) => option.type === ApplicationCommandOptionType.SubCommand
+    );
+
+    switch (subCommand?.name) {
+      case "list":
+        {
+          client.createInteractionResponse(interaction.id, interaction.token, {
+            type: InteractionCallbackType.ChannelMessageWithSource,
+            data: {
+              flags: MessageFlags.Ephemeral,
+              embeds: [
+                {
+                  title: "Blacklist",
+                  description: `The blacklist is a list containing users and roles that will not have any access to the ticketing system. All users and roles will not be able to create tickets, manage panels, etc.`,
+                  fields: [
+                    {
+                      name: "Roles",
+                      value:
+                        blacklistData.rolesIDs.length !== 0
+                          ? `${blacklistData.rolesIDs.map(
+                              (roleID) => `- ${roleMention(roleID)}`
+                            )}`
+                          : "None",
+                      inline: true,
+                    },
+                    {
+                      name: "Users",
+                      value:
+                        blacklistData.usersIDs.length !== 0
+                          ? `${blacklistData.usersIDs.map(
+                              (userID) => `- ${userMention(userID)}`
+                            )}`
+                          : "None",
+                      inline: true,
+                    },
+                  ],
+                },
+              ],
+            },
+          });
+        }
+        break;
+    }
   },
 };
