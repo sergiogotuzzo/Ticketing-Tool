f37f31668c9028b271fd1eca1b8cfb2b75449dd8
Sergio Gotuzzo <sergiotuzzo@gmail.com>
2025-12-02 15:59:08 +0100

    feat: help command

--- src/commands/help.js
+++ src/commands/help.js
@@ -2,10 +2,8 @@ const {
   Client,
   InteractionCallbackType,
   MessageFlags,
-  BitwisePermissionFlags,
   ApplicationCommandOptionType,
-  userMention,
-  roleMention,
+  slashCommandMention,
 } = require("disgroove");
 
 module.exports = {
@@ -17,11 +15,55 @@ module.exports = {
    * @param {import("disgroove").Interaction} interaction
    */
   run: async (client, interaction) => {
+    const cmds = await client.getGlobalApplicationCommands(
+      client.application.id
+    );
+    const filter = (cmd) =>
+      cmd.type === ApplicationCommandOptionType.SubCommand ||
+      cmd.type === ApplicationCommandOptionType.SubCommandGroup;
+
     client.createInteractionResponse(interaction.id, interaction.token, {
-        type: InteractionCallbackType.ChannelMessageWithSource,
-        data: {
-            content: (await client.getGlobalApplicationCommands(client.application.id)).join("\n")
-        }
-    })
+      type: InteractionCallbackType.ChannelMessageWithSource,
+      data: {
+        embeds: [
+          {
+            color: 5793266,
+            title: "Commands' list",
+            description: cmds
+              .map((cmd) =>
+                cmd.options?.filter(filter).length > 0
+                  ? cmd.options
+                      .filter(filter)
+                      .map((subCmd) =>
+                        subCmd.options?.filter(filter).length > 0
+                          ? subCmd.options
+                              .filter(filter)
+                              .map(
+                                (subSubCmd) =>
+                                  `- ${slashCommandMention(
+                                    cmd.name,
+                                    cmd.id,
+                                    subSubCmd.name,
+                                    subCmd.name
+                                  )}: ${subSubCmd.description}`
+                              )
+                              .join("\n")
+                          : `- ${slashCommandMention(
+                              cmd.name,
+                              cmd.id,
+                              subCmd.name
+                            )}: ${subCmd.description}`
+                      )
+                      .join("\n")
+                  : `- ${slashCommandMention(cmd.name, cmd.id)}: ${
+                      cmd.description
+                    }`
+              )
+              .join("\n"),
+          },
+        ],
+        flags: MessageFlags.Ephemeral,
+      },
+    });
   },
 };
